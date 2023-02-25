6e0bb10b23c323b0e5b5569c821e13564f99edd1
Yj621 <wjddbswl26@gmail.com>
2023-02-25 18:56:43 +0900

    Completed Enemy
    Completed Enemy

--- yunji_project_011/Assets/Script/Enemy.cs
+++ yunji_project_011/Assets/Script/Enemy.cs
@@ -2,22 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Enemy : MonoBehaviour
 {
     public int maxHealth;
     public int curHealth;
-   
+    public Transform target;
+    public bool isChase;
 
     Rigidbody rigid;
     BoxCollider boxCollider;
     Material mat;
+    NavMeshAgent nav;
+    Animator anim;
 
     void Awake() //초기화 함수
     {
         rigid= GetComponent<Rigidbody>();
         boxCollider= GetComponent<BoxCollider>();
-        mat = GetComponent<MeshRenderer>().material;
+        mat = GetComponentInChildren<MeshRenderer>().material;
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+
+        Invoke("ChaseStart", 2);
+    }
+
+    void ChaseStart()
+    {
+        isChase = true;
+        anim.SetBool("isWalk", true);
+    }
+   
+    void Update()
+    {
+        if(isChase)
+            nav.SetDestination(target.position);
+    }
+    void FreezeVelocity()
+    {
+        if (isChase)
+        {
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        FreezeVelocity();
     }
 
     void OnTriggerEnter(Collider other)
@@ -77,6 +112,10 @@ public class Enemy : MonoBehaviour
         {
             mat.color = Color.gray;
             gameObject.layer = 14;
+            isChase = false;
+            nav.enabled = false; //사망 리액션 유지
+
+            anim.SetTrigger("doDie");
 
             if(isGrenade)
             {
